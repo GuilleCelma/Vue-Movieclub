b2594101ccc2f1dbc99079ab8d1810c2a061f5f3
Guille Celma <guillecelmaan@hotmail.es>
2021-11-07 03:00:24 +0100

    Feature:added some basic styles to movie page

--- pages/movies.vue
+++ pages/movies.vue
@@ -1,8 +1,8 @@
 <template>
   <div >
       
-    <section id="intro" >
-      <h2>Top Rated Movies!</h2>
+    <section id="intro" class=" flex flex-col items-center" >
+      <h2 class="my-12 text-xl">Top Rated Movies!</h2>
       
       <div class="flex flex-wrap justify-center items-center">
 
